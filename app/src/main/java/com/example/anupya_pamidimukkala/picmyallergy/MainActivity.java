c4fde13f5ccc5b5c22b26f70ba05bdffa9801f0f
Anupya Pamidimukkala <anupya@hotmail.ca>
2018-03-20 14:10:38 -0400

    cannot search

--- app/src/main/java/com/example/anupya_pamidimukkala/picmyallergy/MainActivity.java
+++ app/src/main/java/com/example/anupya_pamidimukkala/picmyallergy/MainActivity.java
@@ -1,5 +1,123 @@
 package com.example.anupya_pamidimukkala.picmyallergy;
 
+/*
+ * Copyright (c) 2015. Thomas Haertel
+ * Multispinner code only. Rest of the code is my own.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // create spinner list elements
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+
+        // JSON parse
+        String json = null;
+        try {
+            InputStream is = getAssets().open("foods.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            JSONArray jArray = new JSONArray(json);
+
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject jo_inside = jArray.getJSONObject(i);
+                adapter.add(jo_inside.getString("foods"));
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        adapter.add("Item1");
+        adapter.add("Item2");
+        adapter.add("Item3");
+        adapter.add("Item4");
+        adapter.add("Item5");
+        */
+
+        // get spinner and set adapter
+        MultiSpinner spinner = (MultiSpinner) findViewById(R.id.spinnerMulti);
+        spinner.setAdapter(adapter, false, onSelectedListener);
+
+        // set initial selection
+        boolean[] selectedItems = new boolean[adapter.getCount()];
+        selectedItems[1] = true; // select second item
+        spinner.setSelected(selectedItems);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private MultiSpinner.MultiSpinnerListener onSelectedListener = new MultiSpinner.MultiSpinnerListener() {
+
+        public void onItemsSelected(boolean[] selected) {
+            // Do something here with the selected items
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < selected.length; i++) {
+                if (selected[i]) {
+                    builder.append(adapter.getItem(i)).append(" ");
+                }
+            }
+
+            Toast.makeText(MainActivity.this, builder.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
+}
+
+/*
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -27,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
-                    */
+
             }
             return false;
         }
@@ -46,3 +164,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+*/
